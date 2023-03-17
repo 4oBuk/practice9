c5e3e0375a2a46c5ff7e9982dfacd8968f9f8ca9
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2023-03-18 01:05:21 +0200

    add another port to access from outside docker

--- src/test/java/com/practice9/Practice9ApplicationTests.java
+++ src/test/java/com/practice9/Practice9ApplicationTests.java
@@ -1,21 +1,13 @@
 package com.practice9;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
-
-import java.util.Arrays;
 
 @SpringBootTest
 class Practice9ApplicationTests {
 
-
-	@Autowired
-	private Environment environment;
-	@Test
-	void contextLoads() {
-		System.out.println(Arrays.toString(environment.getActiveProfiles()));
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
